d38c6acc16342dd5556ac20091a2500760484f88
Darren K. Maczka <dmaczka@vt.edu>
2012-04-02 11:31:29 -0400

    Version 3 of parent/child example

--- multiprogramming/child.cpp
+++ multiprogramming/child.cpp
@@ -1,15 +1,17 @@
-// This is file child.cpp
+// This is file child.cpp - update 2
 #include <sys/wait.h>
 #include <iostream>
 using namespace std;
-int main(int argc, const char* argv[])
+int main(int argc, char *argv[])
 {
+    int i;
+   
     cout << "Process[" << getpid() << "]: Child executing..." << endl;
-    for (int i=0; i<argc; i++)      // print arguments
+    for (i=0; i<argc; i++)      // print arguments
     {
 	cout << argv[i] << endl;
     }
-    for (int i=0; i<2000000000; i++) // busy loop
+    while(1) // busy loop
     {
     }
     cout << "Process[" << getpid() << "]: Child terminating." << endl;
--- multiprogramming/parent.cpp
+++ multiprogramming/parent.cpp
@@ -1,25 +1,26 @@
-// This is file parent.cpp
+// This is file parent.cpp - update 2
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <iostream>
 using namespace std;
 int main()
 {
-    if(fork() == 0) 
+    int pid = fork();
+    if(pid == 0) 
     { 
 	// this is the child process 
 	execl("./child", "child", "red", "white", "blue", NULL);
 	exit(0); // shouldn't ever get here
     }
-     
+   
     // parent process
     cout << "Process[" << getpid() << "]: Parent executing..." << endl;
-    sleep(2); // wait two seconds
-     
-    if(wait(NULL) > 0)
-	cout << "Process[" << getpid() << "]: Parent detects child terminating..."
+    sleep(6); 
+   
+    if(kill(pid, SIGKILL) == 0)
+	cout << "Process[" << getpid() << "]: Parent terminated child process..."
 	     << endl;
-     
+   
     cout << "Process[" << getpid() << "]: Parent terminating." << endl;
     return 0;
 }
